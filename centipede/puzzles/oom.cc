22df8da0babcf7b99868650c94f153ccb0ff4b4a
Kostya Serebryany <kcc@google.com>
2023-06-02 14:48:43 -0700

    #Centipede make the oom puzzle use less RAM, to deflake it.
    PiperOrigin-RevId: 537414810

--- centipede/puzzles/oom.cc
+++ centipede/puzzles/oom.cc
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 // Centipede puzzle: easy-to-reach OOM.
-// RUN: Run && SolutionIs OOM && ExpectOOM
+// RUN: Run --rss_limit_mb=1000 && SolutionIs OOM && ExpectOOM
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
@@ -21,7 +21,7 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   if (size == 3 && data[0] == 'O' && data[1] == 'O' && data[2] == 'M') {
-    size_t huge_mem_size = (4ULL << 30) + (1ULL << 20);  // 4Gb + 1Mb
+    size_t huge_mem_size = (1ULL << 30) + (1ULL << 20);  // 1Gb + 1Mb
     char *ptr = new char[huge_mem_size];  // OOM here or one line below.
     memset(ptr, 42, huge_mem_size);
     fprintf(stderr, "%p\n", ptr);  // so that ptr it's not optimized away.
