5c162f1c6e1b64ca391ed234de5d30e87535c611
FuzzTest Team <fuzztest@google.com>
2023-06-28 20:03:46 -0700

    Internal change
    PiperOrigin-RevId: 544230545

--- centipede/shared_memory_blob_sequence.cc
+++ centipede/shared_memory_blob_sequence.cc
@@ -32,10 +32,70 @@ static void ErrorOnFailure(bool condition, const char *text) {
   abort();
 }
 
-SharedMemoryBlobSequence::SharedMemoryBlobSequence(const char *name,
-                                                   size_t size)
-    : size_(size) {
+BlobSequence::BlobSequence(uint8_t *data, size_t size)
+    : data_(data), size_(size) {
   ErrorOnFailure(size < sizeof(Blob::size), "Size too small");
+}
+
+bool BlobSequence::Write(Blob blob) {
+  ErrorOnFailure(!blob.IsValid(), "Write(): blob.tag must not be zero");
+  ErrorOnFailure(had_reads_after_reset_, "Write(): Had reads after reset");
+  had_writes_after_reset_ = true;
+  if (offset_ + sizeof(blob.size) + sizeof(blob.tag) + blob.size > size_)
+    return false;
+
+  // Write tag.
+  memcpy(data_ + offset_, &blob.tag, sizeof(blob.tag));
+  offset_ += sizeof(blob.tag);
+
+  // Write size.
+  memcpy(data_ + offset_, &blob.size, sizeof(blob.size));
+  offset_ += sizeof(blob.size);
+  // Write data.
+  memcpy(data_ + offset_, blob.data, blob.size);
+  offset_ += blob.size;
+  if (offset_ + sizeof(blob.size) + sizeof(blob.tag) <= size_) {
+    // Write zero tag/size to data_+offset_ but don't change the offset.
+    // This is required to overwrite any stale bits in data_.
+    Blob invalid_blob;  // invalid.
+    memcpy(data_ + offset_, &invalid_blob.tag, sizeof(invalid_blob.tag));
+    memcpy(data_ + offset_ + sizeof(invalid_blob.tag), &invalid_blob.size,
+           sizeof(invalid_blob.size));
+  }
+  return true;
+}
+
+Blob BlobSequence::Read() {
+  ErrorOnFailure(had_writes_after_reset_, "Had writes after reset");
+  had_reads_after_reset_ = true;
+  if (offset_ + sizeof(Blob::size) + sizeof(Blob::tag) >= size_) return {};
+  // Read blob_tag.
+  Blob::SizeAndTagT blob_tag = 0;
+  memcpy(&blob_tag, data_ + offset_, sizeof(blob_tag));
+  offset_ += sizeof(blob_tag);
+  // Read blob_size.
+  Blob::SizeAndTagT blob_size = 0;
+  memcpy(&blob_size, data_ + offset_, sizeof(Blob::size));
+  offset_ += sizeof(Blob::size);
+  // Read blob_data.
+  ErrorOnFailure(offset_ + blob_size > size_, "Not enough bytes");
+  if (blob_tag == 0 && blob_size == 0) return {};
+  ErrorOnFailure(blob_tag == 0, "Read: blob.tag must not be zero");
+  Blob result{blob_tag, blob_size, data_ + offset_};
+  offset_ += result.size;
+  return result;
+}
+
+void BlobSequence::Reset() {
+  offset_ = 0;
+  had_reads_after_reset_ = false;
+  had_writes_after_reset_ = false;
+}
+
+SharedMemoryBlobSequence::SharedMemoryBlobSequence(const char *name,
+                                                   size_t size) {
+  ErrorOnFailure(size < sizeof(Blob::size), "Size too small");
+  size_ = size;
   fd_ = memfd_create(name, MFD_CLOEXEC);
   ErrorOnFailure(fd_ < 0, "memfd_create() failed");
   const size_t path_size =
@@ -69,12 +129,6 @@ SharedMemoryBlobSequence::~SharedMemoryBlobSequence() {
   ErrorOnFailure(close(fd_), "close() failed");
 }
 
-void SharedMemoryBlobSequence::Reset() {
-  offset_ = 0;
-  had_reads_after_reset_ = false;
-  had_writes_after_reset_ = false;
-}
-
 void SharedMemoryBlobSequence::ReleaseSharedMemory() {
   // Setting size to 0 releases the memory to OS.
   ErrorOnFailure(ftruncate(fd_, 0) != 0, "ftruncate(0) failed)");
@@ -86,54 +140,6 @@ size_t SharedMemoryBlobSequence::NumBytesUsed() const {
   struct stat statbuf;
   ErrorOnFailure(fstat(fd_, &statbuf), "fstat() failed)");
   return statbuf.st_blocks * S_BLKSIZE;
-}
-
-bool SharedMemoryBlobSequence::Write(Blob blob) {
-  ErrorOnFailure(!blob.IsValid(), "Write(): blob.tag must not be zero");
-  ErrorOnFailure(had_reads_after_reset_, "Write(): Had reads after reset");
-  had_writes_after_reset_ = true;
-  if (offset_ + sizeof(blob.size) + sizeof(blob.tag) + blob.size > size_)
-    return false;
-  // Write tag.
-  memcpy(data_ + offset_, &blob.tag, sizeof(blob.tag));
-  offset_ += sizeof(blob.tag);
-
-  // Write size.
-  memcpy(data_ + offset_, &blob.size, sizeof(blob.size));
-  offset_ += sizeof(blob.size);
-  // Write data.
-  memcpy(data_ + offset_, blob.data, blob.size);
-  offset_ += blob.size;
-  if (offset_ + sizeof(blob.size) + sizeof(blob.tag) <= size_) {
-    // Write zero tag/size to data_+offset_ but don't change the offset.
-    // This is required to overwrite any stale bits in data_.
-    Blob invalid_blob;  // invalid.
-    memcpy(data_ + offset_, &invalid_blob.tag, sizeof(invalid_blob.tag));
-    memcpy(data_ + offset_ + sizeof(invalid_blob.tag), &invalid_blob.size,
-           sizeof(invalid_blob.size));
-  }
-  return true;
-}
-
-Blob SharedMemoryBlobSequence::Read() {
-  ErrorOnFailure(had_writes_after_reset_, "Had writes after reset");
-  had_reads_after_reset_ = true;
-  if (offset_ + sizeof(Blob::size) + sizeof(Blob::tag) >= size_) return {};
-  // Read blob_tag.
-  Blob::SizeAndTagT blob_tag = 0;
-  memcpy(&blob_tag, data_ + offset_, sizeof(blob_tag));
-  offset_ += sizeof(blob_tag);
-  // Read blob_size.
-  Blob::SizeAndTagT blob_size = 0;
-  memcpy(&blob_size, data_ + offset_, sizeof(Blob::size));
-  offset_ += sizeof(Blob::size);
-  // Read blob_data.
-  ErrorOnFailure(offset_ + blob_size > size_, "Not enough bytes");
-  if (blob_tag == 0 && blob_size == 0) return {};
-  ErrorOnFailure(blob_tag == 0, "Read: blob.tag must not be zero");
-  Blob result{blob_tag, blob_size, data_ + offset_};
-  offset_ += result.size;
-  return result;
 }
 
 }  // namespace centipede
--- centipede/shared_memory_blob_sequence.h
+++ centipede/shared_memory_blob_sequence.h
@@ -48,6 +48,61 @@ struct Blob {
   const uint8_t *data = nullptr;
 };
 
+// The BlobSequence is a consecutive sequence of Blobs.
+class BlobSequence {
+ public:
+  // Creates a new blob sequence of arbitrary `data` with given `size` in bytes,
+  // must be >= 8. Aborts on any failure. The amount of actual data that can be
+  // written is slightly less.
+  explicit BlobSequence(uint8_t *data, size_t size);
+
+  // Writes the contents of `blob` to the blob sequence.
+  // Returns true on success.
+  // Returns false when the blob sequence is full.
+  // A failed Write does not change the internal state.
+  // Must not be called after Read() w/o first calling Reset().
+  bool Write(Blob blob);
+
+  // Writes `tag`/`value` as a blob. `T` should be a POD.
+  // Returns true on success.
+  template <typename T>
+  bool Write(Blob::SizeAndTagT tag, T value) {
+    static_assert(std::is_pod_v<T>, "T must be a POD");
+    return Write(
+        {tag, sizeof(value), reinterpret_cast<const uint8_t *>(&value)});
+  }
+
+  // Reads the next blob from the sequence.
+  // If no more blobs are left, returns a blob with size = 0.
+  // Must not be called after Write() w/o first calling Reset().
+  Blob Read();
+
+  // Resets the internal state, allowing to read from or write to
+  // starting from the beginning of the blob sequence.
+  void Reset();
+
+  // The position after last Write (or last Read).
+  size_t offset() const { return offset_; }
+
+ protected:
+  // Default constructor to create an empty blob sequence. It is used by the
+  // constructors of child classes.
+  explicit BlobSequence() = default;
+
+  // data_ contains a sequence of {size, payload} pairs,
+  // where size is 8 bytes and payload is size bytes.
+  // After writing a blob, we also write 0 in place of the next blob's size,
+  // if there is space left so that to overwrite any stale data left there.
+  uint8_t *data_ = nullptr;
+  size_t size_ = 0;
+
+ private:
+  // offset_ points to the position in data_ after last Write (or last Read).
+  size_t offset_ = 0;
+  bool had_reads_after_reset_ = false;
+  bool had_writes_after_reset_ = false;
+};
+
 // SharedMemoryBlobSequence:
 // enables inter-process communication via shared memory.
 //
@@ -85,7 +140,7 @@ struct Blob {
 //    }
 //  }
 //
-class SharedMemoryBlobSequence {
+class SharedMemoryBlobSequence : public BlobSequence {
  public:
   // Creates a new shared blob sequence with `name` (for debugging only, not an
   // actual path). Aborts on any failure. `size` is the size of the shared
@@ -100,32 +155,6 @@ class SharedMemoryBlobSequence {
   // Releases all resources.
   ~SharedMemoryBlobSequence();
 
-  // Writes the contents of `blob` to shared memory.
-  // Returns true on success.
-  // Returns false when the blob sequence is full.
-  // A failed Write does not change the internal state.
-  // Must not be called after Read() w/o first calling Reset().
-  bool Write(Blob blob);
-
-  // Writes `tag`/`value` as a blob. `T` should be a POD.
-  // Returns true on success.
-  template <typename T>
-  bool Write(Blob::SizeAndTagT tag, T value) {
-    static_assert(std::is_pod_v<T>, "T must be a POD");
-    return Write(
-        {tag, sizeof(value), reinterpret_cast<const uint8_t *>(&value)});
-  }
-
-  // Reads the next blob from the shared memory.
-  // If no more blobs are left, returns a blob with size = 0.
-  // Must not be called after Write() w/o first calling Reset().
-  Blob Read();
-
-  // Resets the internal state, allowing to read from or write to
-  // starting from the beginning of the blob sequence.
-  // Does not affect the contents of the shared memory.
-  void Reset();
-
   // Releases shared memory used by `this`.
   void ReleaseSharedMemory();
 
@@ -138,25 +167,13 @@ class SharedMemoryBlobSequence {
   const char *path() const { return path_; }
 
  private:
-  // mmaps `size_` bytes from `fd_`, assigns to `data_`. Crashes on error.
+  // mmaps `size_` bytes from `fd_`, assigns to `data_`. Crashes if mmap failed.
   void MmapData();
 
   // Will be initialized as a generated internal path or a copy of `path`
   // passed in.
   char path_[PATH_MAX] = {0};
-
-  // pointer to the beginning of the shared memory region.
-  // *data_ contains a sequence of {size, payload} pairs,
-  // where size is 8 bytes and payload is size bytes.
-  // After writing a blob, we also write 0 in place of the next blob's size,
-  // if there is space left so that to overwrite any stale data left there.
-  uint8_t *data_ = nullptr;
-  // offset_ points to the position in data_ after last Write (or last Read).
-  size_t offset_ = 0;
-  size_t size_ = 0;  // size of the shared memory region.
-  int fd_ = 0;       // file descriptor used to mmap the shared memory region.
-  bool had_reads_after_reset_ = false;
-  bool had_writes_after_reset_ = false;
+  int fd_ = 0;  // file descriptor used to mmap the shared memory region.
 };
 
 }  // namespace centipede
