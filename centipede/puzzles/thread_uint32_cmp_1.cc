1d2ce08db7a9d52a0f178434e6db695d10f81708
Kostya Serebryany <kcc@google.com>
2023-08-11 11:27:18 -0700

    #Centipede make puzzles/uint32_cmp_1 more sensitive, add puzzles/thread_uint32_cmp_1
    PiperOrigin-RevId: 556025778

--- centipede/puzzles/thread_uint32_cmp_1.cc
+++ centipede/puzzles/thread_uint32_cmp_1.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 The Centipede Authors.
+// Copyright 2023 The Centipede Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,25 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Centipede puzzle: one 4-byte cmp.
+// Centipede puzzle: one 4-byte cmp, in a separate thread.
 // We should be able to solve it w/o cmp features *or* w/o auto dictionary.
 // RUN: Run && SolutionIs Fuzz
-// RUN: Run --use_cmp_features=0 && SolutionIs Fuzz
 // RUN: Run --use_auto_dictionary=0 && SolutionIs Fuzz
+// TODO(b/295378866): Centipede runner currently fails to collect dictionary
+// entries from merged threads, so if we disable cmp features we can't crack
+// this puzzle. Enable "Run --use_cmp_features=0 && SolutionIs Fuzz"
 
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <thread>  // NOLINT(build/c++11)
 
 // non-const, to avoid compiler optimization.
 static char expected_data[] = "Fuzz";
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  uint32_t value, expected_value;
-  if (size == sizeof(value)) {
-    memcpy(&value, data, sizeof(value));
-    memcpy(&expected_value, expected_data, sizeof(expected_value));
-    if (value == expected_value) abort();
-  }
+  std::thread t([&]() {
+    uint32_t value, expected_value;
+    if (size == sizeof(value)) {
+      memcpy(&value, data, sizeof(value));
+      memcpy(&expected_value, expected_data, sizeof(expected_value));
+      if (value == expected_value) abort();
+    }
+  });
+  t.join();
   return 0;
 }
