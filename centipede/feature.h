1e03a8552a94df5dbbf7db2b1bc080d206d15a68
Filip Niksic <fniksic@google.com>
2023-10-03 08:59:43 -0700

    #Centipede Fix an ODR violation in feature.h.
    `constexpr` global variables must be inlined in header files, otherwise each
    translation unit gets its own copy.
    
    PiperOrigin-RevId: 570403058

--- centipede/feature.h
+++ centipede/feature.h
@@ -160,7 +160,7 @@ inline constexpr Domain kCMPHamming = {__COUNTER__};
 inline constexpr Domain kCMPDiffLog = {__COUNTER__};
 
 // Features derived from observing function call stacks.
-constexpr Domain kCallStack = {__COUNTER__};
+inline constexpr Domain kCallStack = {__COUNTER__};
 // Features derived from computing (bounded) control flow paths.
 inline constexpr Domain kBoundedPath = {__COUNTER__};
 // Features derived from (unordered) pairs of PCs.
