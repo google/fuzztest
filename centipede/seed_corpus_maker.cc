dbd62c40e1d8d03f6d258de696224a14323e9816
Sergey Shevchenko <sergeygs@google.com>
2023-11-09 20:54:34 -0800

    #Centipede Corpus seeder: Improve error handling
    PiperOrigin-RevId: 581112051

--- centipede/seed_corpus_maker.cc
+++ centipede/seed_corpus_maker.cc
@@ -17,7 +17,10 @@
 #include <string>
 
 #include "absl/flags/flag.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "./centipede/config_init.h"
+#include "./centipede/remote_file.h"
 #include "./centipede/seed_corpus_maker_flags.h"
 #include "./centipede/seed_corpus_maker_lib.h"
 #include "./centipede/util.h"
@@ -26,13 +29,20 @@ int main(int argc, char** argv) {
   (void)centipede::config::InitRuntime(argc, argv);
 
   const std::string config = absl::GetFlag(FLAGS_config);
+  QCHECK(!config.empty());
+  const std::string override_out_dir = absl::GetFlag(FLAGS_override_out_dir);
   const std::string binary_path = absl::GetFlag(FLAGS_coverage_binary_path);
+  const std::string binary_name = std::filesystem::path{binary_path}.filename();
+  QCHECK(!binary_name.empty())
+      << "--coverage_binary_path yields empty basename";
   std::string binary_hash = absl::GetFlag(FLAGS_coverage_binary_hash);
-  if (binary_hash.empty() && !binary_path.empty()) {
+  if (binary_hash.empty()) {
+    LOG(INFO) << "--coverage_binary_hash was not provided: computing hash from "
+                 "actual file at --coverage_binary_path";
+    QCHECK(centipede::RemotePathExists(binary_path))
+        << "--coverage_binary_path doesn't exist";
     binary_hash = centipede::HashOfFileContents(binary_path);
   }
-  const std::string binary_name = std::filesystem::path{binary_path}.filename();
-  const std::string override_out_dir = absl::GetFlag(FLAGS_override_out_dir);
 
   centipede::GenerateSeedCorpusFromConfig(  //
       config, binary_name, binary_hash, override_out_dir);
