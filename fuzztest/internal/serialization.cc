9d2f77676f7a47eb1e168c49eafe20d2ef33d3d8
Xinhao Yuan <xinhaoyuan@google.com>
2024-03-27 14:07:02 -0700

    A few (aggressive) improvements on ser/deserialization.
    1. Avoid slow path of vector re-allocation using reserve.
    2. Add a new binary format for corpus, which uses raw memory I/O as much as possible.
    3. Specially handles std::vector<{int8_t,uint8_t,std::byte}> as same as std::string to save space, which is used in llvm_fuzzer_wrapper.
    
    This is mainly to improve the performance for FuzzTest/Centiepde integration, where every input is serialized after mutation and deserialized before execution.
    Will switch the default output format in the later CL, but the old text-based format would still be parsable (by branching on the header).
    
    PiperOrigin-RevId: 619650029

--- fuzztest/internal/serialization.cc
+++ fuzztest/internal/serialization.cc
@@ -17,6 +17,7 @@
 #include <cctype>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <limits>
 #include <optional>
 #include <string>
@@ -130,9 +131,10 @@ bool ReadScalar(std::string& out, absl::string_view value) {
   return true;
 }
 
+constexpr int kMaxParseRecursionDepth = 128;
+
 bool ParseImpl(IRObject& obj, absl::string_view& str, int recursion_depth) {
-  static constexpr int kMaxRecursionDepth = 128;
-  if (recursion_depth > kMaxRecursionDepth) return false;
+  if (recursion_depth > kMaxParseRecursionDepth) return false;
   absl::string_view key = ReadToken(str);
   if (key.empty() || key == "}") {
     // The object is empty. Put the token back and return.
@@ -169,9 +171,154 @@ bool ParseImpl(IRObject& obj, absl::string_view& str, int recursion_depth) {
   }
 }
 
+// NOTE: Binary format assumes the same endianness between producers and
+// consumers - we assume little-endianness for now.
+
+enum class BinaryFormatHeader : char {
+  kEmpty = 0,
+  kUInt64,
+  kDouble,
+  kString,
+  kObject,
+};
+
+struct BinaryOutputVisitor {
+  char* buf;
+  size_t& offset;
+
+  void operator()(std::monostate) const {
+    if (buf) {
+      buf[offset] = static_cast<char>(BinaryFormatHeader::kEmpty);
+    }
+    offset += 1;
+  }
+
+  void operator()(uint64_t value) const {
+    if (buf) {
+      buf[offset] = static_cast<char>(BinaryFormatHeader::kUInt64);
+      std::memcpy(buf + offset + 1, &value, sizeof(value));
+    }
+    offset += 1 + sizeof(value);
+  }
+
+  void operator()(double value) const {
+    if (buf) {
+      buf[offset] = static_cast<char>(BinaryFormatHeader::kDouble);
+      std::memcpy(buf + offset + 1, &value, sizeof(value));
+    }
+    offset += 1 + sizeof(value);
+  }
+
+  void operator()(const std::string& value) const {
+    const uint64_t size = value.size();
+    if (buf) {
+      buf[offset] = static_cast<char>(BinaryFormatHeader::kString);
+      std::memcpy(buf + offset + 1, &size, sizeof(size));
+      std::memcpy(buf + offset + 1 + sizeof(size), value.data(), size);
+    }
+    offset += 1 + sizeof(size) + size;
+  }
+
+  void operator()(const std::vector<IRObject>& value) const {
+    const uint64_t size = value.size();
+    if (buf) {
+      buf[offset] = static_cast<char>(BinaryFormatHeader::kObject);
+      std::memcpy(buf + offset + 1, &size, sizeof(size));
+    }
+    offset += 1 + sizeof(size);
+    for (const auto& sub : value) {
+      std::visit(BinaryOutputVisitor{buf, offset}, sub.value);
+    }
+  }
+};
+
+constexpr absl::string_view kBinaryHeader = "FUZZTESTv1b";
+
+struct BinaryParseBuf {
+  const char* str;
+  size_t size;
+
+  inline bool empty() const { return size == 0; }
+  inline void Advance(size_t s) {
+    if (s > size) s = size;
+    str += s;
+    size -= s;
+  }
+};
+
+bool BinaryParse(IRObject& obj, BinaryParseBuf& buf, int recursion_depth) {
+  if (recursion_depth > kMaxParseRecursionDepth) return false;
+  if (buf.empty()) return false;
+  const auto h = static_cast<BinaryFormatHeader>(buf.str[0]);
+  buf.Advance(1);
+  switch (h) {
+    case BinaryFormatHeader::kEmpty: {
+      return true;
+    }
+    case BinaryFormatHeader::kUInt64: {
+      if (buf.size < sizeof(uint64_t)) return false;
+      auto& t = obj.value.emplace<uint64_t>();
+      std::memcpy(&t, buf.str, sizeof(uint64_t));
+      buf.Advance(sizeof(uint64_t));
+      return true;
+    }
+    case BinaryFormatHeader::kDouble: {
+      if (buf.size < sizeof(double)) return false;
+      auto& t = obj.value.emplace<double>();
+      std::memcpy(&t, buf.str, sizeof(t));
+      buf.Advance(sizeof(double));
+      return true;
+    }
+    case BinaryFormatHeader::kString: {
+      if (buf.size < sizeof(uint64_t)) return false;
+      uint64_t str_size;
+      std::memcpy(&str_size, buf.str, sizeof(str_size));
+      buf.Advance(sizeof(uint64_t));
+      if (buf.size < str_size) return false;
+      obj.value.emplace<std::string>() = {buf.str, str_size};
+      buf.Advance(str_size);
+      return true;
+    }
+    case BinaryFormatHeader::kObject: {
+      if (buf.size < sizeof(uint64_t)) return false;
+      uint64_t vec_size;
+      std::memcpy(&vec_size, buf.str, sizeof(vec_size));
+      buf.Advance(sizeof(vec_size));
+      auto& v = obj.value.emplace<std::vector<IRObject>>();
+      v.reserve(vec_size);
+      for (uint64_t i = 0; i < vec_size; ++i) {
+        if (!BinaryParse(v.emplace_back(), buf, recursion_depth + 1))
+          return false;
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
+bool IsInBinaryFormat(absl::string_view str) {
+  // Not using absl::string_view or std::memcmp because they could be
+  // instrumented and using them could pollute coverage.
+  return str.size() >= kBinaryHeader.size() &&
+         __builtin_memcmp(str.data(), kBinaryHeader.data(),
+                          kBinaryHeader.size()) == 0;
+}
+
 }  // namespace
 
-std::string IRObject::ToString() const {
+std::string IRObject::ToString(bool binary_format) const {
+  if (binary_format) {
+    size_t offset = kBinaryHeader.size();
+    // Determine the output size before writing to the output to avoid
+    // reallocation.
+    std::visit(BinaryOutputVisitor{/*buf=*/nullptr, offset}, value);
+    std::string out;
+    out.resize(offset);
+    std::memcpy(out.data(), kBinaryHeader.data(), kBinaryHeader.size());
+    offset = kBinaryHeader.size();
+    std::visit(BinaryOutputVisitor{out.data(), offset}, value);
+    return out;
+  }
   std::string out = absl::StrCat(kHeader, "\n");
   std::visit(OutputVisitor{value.index(), 0, out}, value);
   return out;
@@ -180,9 +327,17 @@ std::string IRObject::ToString() const {
 // TODO(lszekeres): Return StatusOr<IRObject>.
 std::optional<IRObject> IRObject::FromString(absl::string_view str) {
   IRObject object;
-  if (ReadToken(str) != kHeader) return std::nullopt;
-  if (!ParseImpl(object, str, /*recursion_depth=*/0) || !ReadToken(str).empty())
-    return std::nullopt;
+  if (IsInBinaryFormat(str)) {
+    BinaryParseBuf buf = {str.data(), str.size()};
+    buf.Advance(kBinaryHeader.size());
+    if (!BinaryParse(object, buf, /*recursion_depth=*/0) || !buf.empty())
+      return std::nullopt;
+  } else {
+    if (ReadToken(str) != kHeader) return std::nullopt;
+    if (!ParseImpl(object, str, /*recursion_depth=*/0) ||
+        !ReadToken(str).empty())
+      return std::nullopt;
+  }
   return object;
 }
 
