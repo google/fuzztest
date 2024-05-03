b603970bc0443e8e6ba183b20ee14784a26b909b
Filip Niksic <fniksic@google.com>
2024-05-03 15:27:07 -0700

    Execv to Centipede in multi-process mode if FUZZTEST_CENTIPEDE_BINARY is set.
    PiperOrigin-RevId: 630513297

--- fuzztest/init_fuzztest.cc
+++ fuzztest/init_fuzztest.cc
@@ -1,6 +1,11 @@
 #include "./fuzztest/init_fuzztest.h"
 
+#if defined(__linux__)
+#include <unistd.h>
+#endif
+
 #include <algorithm>
+#include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -24,6 +29,7 @@
 #include "./fuzztest/internal/flag_name.h"
 #include "./fuzztest/internal/googletest_adaptor.h"
 #include "./fuzztest/internal/io.h"
+#include "./fuzztest/internal/logging.h"
 #include "./fuzztest/internal/registry.h"
 #include "./fuzztest/internal/runtime.h"
 
@@ -179,6 +185,43 @@ internal::Configuration CreateConfigurationsFromFlags(
       time_limit_per_test};
 }
 
+#if defined(__linux__)
+
+void ExecvToCentipede(const char* centipede_binary, int argc, char** argv) {
+  std::string binary_arg = absl::StrCat("--binary=", argv[0]);
+  for (int i = 1; i < argc; ++i) {
+    absl::StrAppend(&binary_arg, " ", argv[i]);
+  }
+  // Additionally we need to append the parsed flags because Abseil removes
+  // them from `argv`. We only append flags with a non-default value.
+  for (const auto [flag_name, flag] : absl::GetAllFlags()) {
+    if (flag->CurrentValue() == flag->DefaultValue()) continue;
+    absl::StrAppend(&binary_arg, " --", flag_name, "=", flag->CurrentValue());
+  }
+  // `execv` guarantees it will not modify the passed arguments, so the
+  // const_casts are OK.
+  char* const args[] = {
+      const_cast<char*>(centipede_binary),
+      const_cast<char*>(binary_arg.c_str()),
+      nullptr,
+  };
+  const int execv_ret = execv(centipede_binary, args);
+  FUZZTEST_INTERNAL_CHECK(false, "execv() should never return. It returned ",
+                          execv_ret, " with an error: ", std::strerror(errno));
+}
+
+#else
+
+void ExecvToCentipede(const char*, int, char**) {
+  FUZZTEST_INTERNAL_CHECK(
+      false,
+      "Switching to Centipede via FUZZTEST_CENTIPEDE_BINARY only works on "
+      "Linux. Please run Centipede manually and use the --binary flag to pass "
+      "the current binary.");
+}
+
+#endif  // defined(__linux__)
+
 }  // namespace
 
 void RunSpecifiedFuzzTest(std::string_view binary_id, std::string_view name) {
@@ -195,6 +238,12 @@ void RunSpecifiedFuzzTest(std::string_view binary_id, std::string_view name) {
 }
 
 void InitFuzzTest(int* argc, char*** argv) {
+  const char* centipede_binary = std::getenv("FUZZTEST_CENTIPEDE_BINARY");
+  const bool is_runner_mode = std::getenv("CENTIPEDE_RUNNER_FLAGS");
+  if (centipede_binary != nullptr && !is_runner_mode) {
+    ExecvToCentipede(centipede_binary, *argc, *argv);
+  }
+
   const bool is_listing = absl::GetFlag(FUZZTEST_FLAG(list_fuzz_tests));
   if (is_listing) {
     for (const auto& name : ListRegisteredTests()) {
