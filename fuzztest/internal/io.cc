0d3618e672571d20d781d6bde78fe1a9133ed041
Hadi Ravanbakhsh <ravanbakhsh@google.com>
2024-06-20 13:05:28 -0700

    Follow symlinks for recursively traversing directories.
    PiperOrigin-RevId: 645130998

--- fuzztest/internal/io.cc
+++ fuzztest/internal/io.cc
@@ -142,8 +142,9 @@ std::vector<std::string> ListDirectoryRecursively(absl::string_view path) {
 
   const std::filesystem::path fs_path{
       std::string_view{path.data(), path.size()}};
-  for (const auto& entry :
-       std::filesystem::recursive_directory_iterator(fs_path)) {
+  for (const auto& entry : std::filesystem::recursive_directory_iterator(
+           fs_path,
+           std::filesystem::directory_options::follow_directory_symlink)) {
     output_paths.push_back(entry.path().string());
   }
   return output_paths;
