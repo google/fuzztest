fdf8759cdf507e50b21493951d460b491fbb6c13
Filip Niksic <fniksic@google.com>
2025-10-15 13:35:21 -0700

    Extract `MockFactory` as `NonOwningCallbacksFactory` for better reusability.
    PiperOrigin-RevId: 819892786

--- centipede/centipede_callbacks.h
+++ centipede/centipede_callbacks.h
@@ -15,8 +15,10 @@
 #ifndef THIRD_PARTY_CENTIPEDE_CENTIPEDE_CALLBACKS_H_
 #define THIRD_PARTY_CENTIPEDE_CENTIPEDE_CALLBACKS_H_
 
+#include <atomic>
 #include <cstddef>
 #include <filesystem>  // NOLINT
+#include <memory>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -33,6 +35,7 @@
 #include "./centipede/shared_memory_blob_sequence.h"
 #include "./centipede/util.h"
 #include "./common/defs.h"
+#include "./common/logging.h"
 
 namespace fuzztest::internal {
 
@@ -220,6 +223,35 @@ class DefaultCallbacksFactory : public CentipedeCallbacksFactory {
     return new Type(env);
   }
   void destroy(CentipedeCallbacks *callbacks) override { delete callbacks; }
+};
+
+// An implementation of `CentipedeCallbacksFactory` that always returns the same
+// predefined `CentipedeCallbacks` object and never destroys it. The factory
+// ensures that each `create()` call must be followed by a `destroy()` call
+// before `create()` can be called again.
+class NonOwningCallbacksFactory : public CentipedeCallbacksFactory {
+ public:
+  explicit NonOwningCallbacksFactory(CentipedeCallbacks& callbacks)
+      : callbacks_(callbacks) {}
+  CentipedeCallbacks* absl_nonnull create(const Environment& env) override {
+    const bool was_already_created =
+        is_created_.exchange(true, std::memory_order_acq_rel);
+    FUZZTEST_CHECK(!was_already_created)
+        << "create() called before destroy() that matches the previous "
+           "create()";
+    return &callbacks_;
+  }
+  void destroy(CentipedeCallbacks* callbacks) override {
+    FUZZTEST_CHECK_EQ(callbacks, &callbacks_);
+    const bool was_created =
+        is_created_.exchange(false, std::memory_order_acq_rel);
+    FUZZTEST_CHECK(was_created)
+        << "destroy() called before the matching create()";
+  }
+
+ private:
+  std::atomic<bool> is_created_ = false;
+  CentipedeCallbacks& callbacks_;
 };
 
 // Creates a CentipedeCallbacks object in CTOR and destroys it in DTOR.
---
+++ centipede/centipede_callbacks_test.cc
@@ -0,0 +1,71 @@
+// Copyright 2025 The Centipede Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "./centipede/centipede_callbacks.h"
+
+#include <string_view>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "./centipede/environment.h"
+#include "./centipede/runner_result.h"
+#include "./common/defs.h"
+
+namespace fuzztest::internal {
+namespace {
+
+class FakeCallbacks : public CentipedeCallbacks {
+ public:
+  explicit FakeCallbacks(const Environment& env) : CentipedeCallbacks(env) {}
+  bool Execute(std::string_view binary, const std::vector<ByteArray>& inputs,
+               BatchResult& batch_result) override {
+    return true;
+  }
+};
+
+TEST(NonOwningCallbacksFactoryTest, CreateReturnsUnderlyingCallbacks) {
+  Environment env;
+  FakeCallbacks callbacks(env);
+  NonOwningCallbacksFactory factory(callbacks);
+  EXPECT_EQ(factory.create(env), &callbacks);
+}
+
+TEST(NonOwningCallbacksFactoryTest, CannotCreateTwice) {
+  Environment env;
+  FakeCallbacks callbacks(env);
+  NonOwningCallbacksFactory factory(callbacks);
+  factory.create(env);
+  EXPECT_DEATH(factory.create(env), "create\\(\\) called before destroy\\(\\)");
+}
+
+TEST(NonOwningCallbacksFactoryTest, CannotDestroyBeforeCreate) {
+  Environment env;
+  FakeCallbacks callbacks(env);
+  NonOwningCallbacksFactory factory(callbacks);
+  EXPECT_DEATH(factory.destroy(&callbacks),
+               "destroy\\(\\) called before the matching create\\(\\)");
+}
+
+TEST(NonOwningCallbacksFactoryTest, CannotDestroyTwice) {
+  Environment env;
+  FakeCallbacks callbacks(env);
+  NonOwningCallbacksFactory factory(callbacks);
+  factory.create(env);
+  factory.destroy(&callbacks);
+  EXPECT_DEATH(factory.destroy(&callbacks),
+               "destroy\\(\\) called before the matching create\\(\\)");
+}
+
+}  // namespace
+}  // namespace fuzztest::internal
