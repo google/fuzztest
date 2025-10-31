121a33ca2236f4ac48040117d48fd4bff5323de2
Filip Niksic <fniksic@google.com>
2025-10-31 13:51:38 -0700

    Use the bug ID from crashing input filename when registering reproducer tests.
    PiperOrigin-RevId: 826616432

--- fuzztest/internal/googletest_adaptor.cc
+++ fuzztest/internal/googletest_adaptor.cc
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <string>
+#include <string_view>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -11,6 +12,8 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
+#include "./common/crashing_input_filename.h"
+#include "./common/logging.h"
 #include "./fuzztest/internal/configuration.h"
 #include "./fuzztest/internal/corpus_database.h"
 #include "./fuzztest/internal/flag_name.h"
@@ -73,8 +76,17 @@ void RegisterSeparateRegressionTestForEachCrashingInput(
   for (const std::string& input : crash_inputs) {
     Configuration updated_configuration = configuration;
     updated_configuration.crashing_input_to_reproduce = input;
-    const std::string suffix =
-        absl::StrCat("/Regression/", std::string(Basename(input)));
+    absl::string_view file_name = Basename(input);
+    auto components = ParseCrashingInputFilename(
+        std::string_view{file_name.data(), file_name.size()});
+    if (!components.ok()) {
+      FUZZTEST_LOG(WARNING)
+          << "Failed to parse crashing input filename " << file_name
+          << ". Not registering a regression test for it. Status: "
+          << components.status();
+      continue;
+    }
+    const std::string suffix = absl::StrCat("/Regression/", components->bug_id);
     RegisterFuzzTestAsGTest<T>(argc, argv, test, updated_configuration, suffix);
   }
 }
