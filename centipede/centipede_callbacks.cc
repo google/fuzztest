f0a421a9d75b08c53a4a6ca59681a20f77e5bb07
Xinhao Yuan <xinhaoyuan@google.com>
2026-01-07 07:40:07 -0800

    Clarify the log message on batch cleanup failure.
    The previous message indicates timeout, which is not always the reason why the wait fails - there could be other low-level reasons such as broken pipes.
    
    PiperOrigin-RevId: 853256871

--- centipede/centipede_callbacks.cc
+++ centipede/centipede_callbacks.cc
@@ -500,11 +500,12 @@ int CentipedeCallbacks::RunBatchForBinary(std::string_view binary) {
   if (should_clean_up) {
     exit_code = [&] {
       if (!cmd.is_executing()) return EXIT_FAILURE;
-      FUZZTEST_LOG(ERROR) << "Cleaning up the batch execution.";
+      FUZZTEST_LOG(ERROR) << "Cleaning up the batch execution with timeout: "
+                          << kCommandCleanupTimeout;
       cmd.RequestStop();
       const auto ret = cmd.Wait(absl::Now() + kCommandCleanupTimeout);
       if (ret.has_value()) return *ret;
-      FUZZTEST_LOG(ERROR) << "Batch execution cleanup failed to end in 60s.";
+      FUZZTEST_LOG(ERROR) << "Failed to wait for the batch execution cleanup.";
       return EXIT_FAILURE;
     }();
     command_contexts_.erase(
