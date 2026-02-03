54dfec04d5c9ad1f22b08002ab6a5e2d0de77671
FuzzTest Team <fuzztest@google.com>
2026-02-03 02:19:00 -0800

    No public description
    PiperOrigin-RevId: 864770469

--- centipede/testing/async_failing_target.cc
+++ centipede/testing/async_failing_target.cc
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdio>
 #include <cstdlib>
+#include <functional>
+#include <vector>
 
+#include "absl/base/nullability.h"
 #include "./centipede/runner_interface.h"
 #include "./common/defs.h"
 
