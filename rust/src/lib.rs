e12f5007b3b0eee50e72f4f87c33f8f14d6d3fd8
Shashank Sharma <theshank@google.com>
2026-08-03 14:04:00 -0700

    No public description
    PiperOrigin-RevId: 958566576

---
+++ rust/src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(cfg_sanitize)]
+
+mod crash_handler;
+pub mod domains;
+pub mod internal;
+pub mod worker;
+
+pub use fuzztest_macro::fuzztest;
+pub use serde;
+
+pub mod options;
+pub use options::get_fuzztest_options;
+pub use options::FuzzTestOptions;
+
+// Re-export helper crates used in macro expansion to decouple downstream Cargo.toml files.
+#[doc(hidden)]
+pub mod reexports {
+    pub use anyhow;
+    pub use inventory;
+    pub use rand;
+}
