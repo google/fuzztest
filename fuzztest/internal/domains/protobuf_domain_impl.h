195588d628d31a2209471ff847e2096921f2a244
FuzzTest Team <fuzztest@google.com>
2026-09-18 16:11:43 -0700

    No public description
    PiperOrigin-RevId: 984120623

--- fuzztest/internal/domains/protobuf_domain_impl.h
+++ fuzztest/internal/domains/protobuf_domain_impl.h
@@ -1218,7 +1218,8 @@ class ProtobufDomainUntypedImpl
         auto v = self.GetValue(corpus_copy);
         // We need to roundtrip through serialization to really dedup. The
         // reflection API alone doesn't cut it.
-        v->ParsePartialFromString(v->SerializePartialAsString());
+        FUZZTEST_CHECK(
+            v->ParsePartialFromString(v->SerializePartialAsString()));
         if (v->GetReflection()->FieldSize(*v, field) ==
             domain.GetValue(copy).size()) {
           // The number of entries is the same, so accept the change.
